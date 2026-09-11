2872afc486d42a03d9c2fb1f7e32298bdfd175ba
Sushant <sushantac@gmail.com>
2026-09-11 21:39:32 +1000

    fix(config): remove unused bootstrap.servers property

--- src/main/java/com/ecommerce/admin/config/KafkaConfig.java
+++ src/main/java/com/ecommerce/admin/config/KafkaConfig.java
@@ -1,16 +1,12 @@
 package com.ecommerce.admin.config;
 
 import org.apache.kafka.clients.admin.NewTopic;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.TopicBuilder;
 
 @Configuration
 public class KafkaConfig {
-
-    @Value("${spring.kafka.bootstrap.servers}")
-    private String bootstrapServers;
 
     @Bean
     public NewTopic orderPlacedTopic() {
